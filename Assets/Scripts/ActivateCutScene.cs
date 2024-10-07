10575193d52831859a9a0ddd7c2e571ad0066fd0
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-07 11:29:12 -0300

    sucesso

---
+++ Assets/Scripts/ActivateCutScene.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//adicionar esse import
+using UnityEngine. Playables;
+
+public class ActivateCutScene : MonoBehaviour
+{
+    [SerializeField] private PlayableDirector playableDirector;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+           playableDirector.Play();
+            GetComponent<BoxCollider>().enabled = false;
+        }
+                
+    }
+}
